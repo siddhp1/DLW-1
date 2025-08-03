06dd1accf323f6a27852bc014aeb8a5f5d13400e
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2025-08-03 10:06:32 -0400

    Create new sample program

---
+++ SAMPLE-PROGRAM.md
@@ -0,0 +1,15 @@
+# Countdown Program
+
+This sample program counts down from 5, subtracting 1 each CPU cycle.
+
+```
+10 05 : Load value at address 16 into register A
+11 45 : Load value at address 17 into register B
+00 42 : Subtract value in register B from value in register A and store the result in register A
+FE 1D : Jump to subtraction instruction if result was non-zero
+12 07 : Store value in register A at address 18 (will store 0)
+FF 08 : Halt program
+00 00 : Padding for code segment
+00 00 : Padding for code segment
+05 01 : Values 5 and 1
+```
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,44 +1,11 @@
 #include <iostream>
 
-#include "dlw1/cpu.hpp"
 #include "dlw1/emulator.hpp"
-#include "dlw1/instruction.hpp"
-
-// Repurpose this function to test writing a program to memory
-void TestDecode() {
-  CPU cpu;
-  std::string input;
-  uint16_t raw;
-
-  while (true) {
-    std::cout << "Enter a 16-bit instruction (hex prefix with 0x, binary with "
-                 "0b, or 'q' to quit)"
-              << std::endl;
-    std::getline(std::cin, input);
-
-    if (input == "q" || input == "quit") {
-      return;
-    }
-
-    if (input.substr(0, 2) == "0x") {
-      raw = static_cast<uint16_t>(std::stoul(input, nullptr, 16));
-    } else if (input.substr(0, 2) == "0b") {
-      raw = static_cast<uint16_t>(std::stoul(input.substr(2), nullptr, 2));
-    } else {
-      raw = static_cast<uint16_t>(std::stoul(input));
-    }
-
-    Instruction ins = cpu.Decode(raw);
-    std::cout << ins;
-  }
-}
 
 int main(void) {
-  // TestDecode();
+  Emulator emulator{};
 
-  Emulator emulator;
-
-  emulator.LoadProgram("program.bin");
+  emulator.LoadProgram("sample_program.bin");
   emulator.Run();
 
   return 0;
